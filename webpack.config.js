cc02182d185806004fefe6cb1262dbbef1057c5c
Joseph <riverfish@gmail.com>
2017-05-03 16:42:24 +0200

    setup for heroku

--- webpack.config.js
+++ webpack.config.js
@@ -31,8 +31,7 @@ module.exports = {
   },
   plugins: [
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV':
-      JSON.stringify(process.env.NODE_ENV)
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
     })
   ]
 };
